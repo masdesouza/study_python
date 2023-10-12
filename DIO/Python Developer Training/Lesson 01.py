4fce816237b09cda0ac54fc9869ed5118765e463
Marcos Souza <masdesouza@gmail.com>
2023-10-12 14:13:27 -0300

    Lesson 01 - DIO Python Developer Training

---
+++ DIO/Python Developer Training/Lesson 01.py
@@ -0,0 +1,90 @@
+#Lesson 01
+print("Oi, seja bem vindo ao curso de Python da DIO")
+
+#Lesson 02 - Data types
+
+#String
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+print(type("Hello World"))
+
+#Integer
+print(123456)
+print(type(123456))
+
+#Float  - Decimal
+print(123.456)
+print(type(123.456))
+
+#Boolean
+print(True)
+print(False)
+print(type(True))
+
+#Complex
+print(complex(5, 10))
+print(type(complex(5, 10)))
+
+#list
+print([10, 20, 30, 40, 50])
+print(type([10, 20, 30, 40, 50]))
+
+#tuple
+print((10, 20, 30, 40, 50))
+print(type((10, 20, 30, 40, 50)))
+
+#range
+print(range(0, 10))
+print(type(range(0, 10)))
+
+#dict
+print({"nome": "João", "idade": 25})
+print(type({"nome": "João", "idade": 25}))
+
+#set
+print({10, 20, 30, 40, 50})
+print(type({10, 20, 30, 40, 50}))
+
+#frozenset
+print(frozenset({10, 20, 30, 40, 50}))
+print(type(frozenset({10, 20, 30, 40, 50})))
+
+#Bytes
+#print(b"João")
+#print(type(b"João"))    
+
+
+#Bytearray
+#print(bytearray(b"João"))
+#print(type(bytearray(b"João")))
+print(bytearray(5))
+print(type(bytearray(5)))
+
+#Memoryview
+#print(memoryview(b"João"))
+#print(type(memoryview(b"João")))
+print(memoryview(bytearray(5)))
+print(type(memoryview(bytearray(5))))
+
+#Iterable
+print(type(range(0, 10)))
+print(type([10, 20, 30, 40, 50]))
+
+
+#Lesson 03 - Variables
+
+#Lesson 04 - Variables
+
+
+
+
+
+
+
+
+
+
+
+
