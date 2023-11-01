779b99985abdcfa3320d37f62d32e581dcadfddb
Marcos Souza <masdesouza@gmail.com>
2023-11-01 13:53:41 -0300

    Study about operation types in Python

---
+++ DIO/Python Developer Training/Lesson 05.py
@@ -0,0 +1,75 @@
+'''
+Study about operation types in Python
+'''
+
+# Arithmetic Operators
+# +, -, *, /, %, **, //
+# + Addition
+print(2 + 2)
+# - Subtraction
+print(2 - 2)
+# * Multiplication
+print(2 * 2)
+# / Division
+print(2 / 2)
+# % Modulus
+print(3 % 2)
+# ** Exponent
+print(2 ** 2)
+# // Floor Division
+print(3 // 2)
+
+# Assignment Operators
+name = 'DIO'
+print(name)
+name += ' Brando '
+print(name)
+name *= 3
+print(name)
+
+
+
+# Comparison Operators
+# ==, !=, >, <, >=, <=
+print(2 == 2)  # True
+print(2 != 2)  # False
+print(2 > 2)   # False
+print(2 < 2)   # False
+print(2 >= 2)  # True
+print(2 <= 2)  # True
+
+# Logical Operators
+# and, or, not
+print(True and False)  # False
+print(False and True)  # False
+print(True or False)   # True
+print(not False)       # True
+
+# Identity Operators
+# is, is not
+print(2 is 2)  # True
+print(2 is not 2)  # False
+
+# Membership Operators
+# in, not in
+name = 'DIO Brando'
+print('DIO' in name)  # True
+print('DIO' not in name)  # False
+
+# Bitwise Operators
+# &, |, ^, ~+, <<, >>
+print(True & False) # Bitwise AND
+print(True | False) # Bitwise OR
+print(True ^ False) # Bitwise XOR
+print(~False) # -1 Bitiwse NOT
+print(True << 2) # 4 Bitwise Left Shift
+print(True >> 2) # 0 Bitwise Right Shift
+
+#Bitwise Operators with assigment
+
+
+
+
+
+
+
