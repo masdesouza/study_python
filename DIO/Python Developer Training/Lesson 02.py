fec2c23ec4db2fe2b28251eb4fd3b1425fd79a8b
Marcos Souza <masdesouza@gmail.com>
2023-10-31 23:20:29 -0300

    Update 02.py

--- DIO/Python Developer Training/Lesson 02.py
+++ DIO/Python Developer Training/Lesson 02.py
@@ -5,19 +5,42 @@ Author: Marcos Souza
 #Variables
 
 #Attributing values to variables using in one line
-conta, nome, saldo, limite = 0, "", 0.0, 0.0
+account, name, balance, limit = 0, "", 0.0, 0.0
 
 #Attributing values to variables using in multiple lines (recommended). using input function.
-conta = input("Digite o número da conta: ")
-nome = input("Digite o nome do cliente: ")
-saldo = float(input("Digite o saldo da conta: "))
-limite = float(input("Digite o limite da conta: "))
-print("O número da conta é:", conta)
-print("O nome do cliente é:", nome)
-print("O saldo da conta é:", saldo)
-print("O limite da conta é:", limite)
+account = input("Enter account number: ")
+name = input("Enter the customer's name: ")
+balance = float(input("Enter the account balance: "))
+limit = float(input("Enter the account limit: "))
 
-saldo = 'marcos'
+print("The account number is:", account)
+print(f"The account number is: {account}")
+print("The customer's name is:", name)
+print(f"The customer's name is: {name}")
+print("The account balance is:", balance)
+print(f"The account balance is: {balance}")
+print("The account limit is:", limit)
+print(f"The account limit is: {limit}")
+
 
 #Constants
+
+#Defining constants
+#The convention is to use all letters in uppercase
+#The convention is to use _ to separate words
+
+#Using in one line
+PI = 3.1415
+GRAVITY = 9.8
+EULER = 2.7182
+STATES = ["São Paulo", "Rio de Janeiro", "Minas Gerais", "Bahia", "Ceará"]
+STATES_TUPLE = ("São Paulo", "Rio de Janeiro", "Minas Gerais", "Bahia", "Ceará")
+STATES_DICT = {"SP": "São Paulo", "RJ": "Rio de Janeiro", "MG": "Minas Gerais", "BA": "Bahia", "CE": "Ceará"}
+
+
+
+
+
+
+
 
