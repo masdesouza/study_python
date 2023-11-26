69655eac64a7a54d0a497cee793041a0490c2655
Marcos Souza <masdesouza@gmail.com>
2023-11-25 23:44:47 -0300

    Using request library to interact with API's

---
+++ Study_api/Example_Study_API.py
@@ -0,0 +1,36 @@
+''' Example API
+    We want to get data from an API? Try this one: https://economia.awesomeapi.com.br/json/all
+    It returns a JSON with the exchange rate of the Brazilian Real (BRL) to all other currencies.
+    DOC = https://docs.awesomeapi.com.br/
+    API dp IBGE = https://servicodados.ibge.gov.br/api/docs
+    API do Twillo = https://www.twilio.com/docs/sms/quickstart/python
+'''
+# Use to interact with API's
+# API to get data from AwesomeAPI
+#Install requests - pip install requests
+import requests
+import json
+
+exchange = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+print(exchange.status_code)
+exchange_json = exchange.json()
+print(exchange_json)
+exchange_USD = exchange_json['USDBRL']['bid']
+print(exchange_USD)
+exchange_EUR = exchange_json['EURBRL']['bid']
+print(exchange_EUR)
+exchange_BTC = exchange_json['BTCBRL']['bid']   
+print(exchange_BTC)
+
+
+# API to post data from Twillo
+#Install Twillo - pip install twilio
+
+
+
+
+
+
+
+
+
---
+++ Study_api/Study_Request_Python.py
@@ -0,0 +1,76 @@
+# Use to interact with API's
+import requests
+
+# Get data - GET request
+'''
+# API to get data from AwesomeAPI
+link = 'https://economia.awesomeapi.com.br/json/all'
+#link = 'https://economia.awesomeapi.com.br/json/last/:moedas'
+req = requests.get(link)
+
+print(req)
+print(req.json())
+print(req.json()['USD'])
+link = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
+req = requests.get(link)
+print(req.json())
+
+'''
+
+#Examples of API's using Firebase 
+# https://firebase.google.com/docs/reference/rest/database
+
+# Create realtime database in Firebase
+link = 'https://testresquestpython-default-rtdb.firebaseio.com/.json' # link to database
+
+#Get data from database
+req = requests.get(link) 
+print(req.json())
+
+
+#Create data in database
+# Post data - POST request
+requests.post(link , json= {'page' : 1,'name':  'Silvana', 'age': 38})                           
+requests.post(link , json= {'page' : 2,'name': 'Pedro', 'age': 16})
+requests.post(link , json= {'page' : 3,'name': 'Marcos', 'age': 42})
+
+req = requests.get(link) 
+print(req.json())
+
+#Get data with parameters - GET request
+params = {'page': 1}
+req = requests.get(link, params = params)
+response = req.json()
+keys = list(response.keys())
+
+     
+#Update data partially in database
+# Patch data - PATCH request
+link_patch = 'https://testresquestpython-default-rtdb.firebaseio.com/'+keys[0]+'.json'
+print(link_patch)
+data = '{"name": "Silvana Amaral de Souza"}'
+req = requests.patch(link_patch, data=data)
+req = requests.get(link)
+print(req.json())
+
+# Delete data - DELETE request
+link_delete = 'https://testresquestpython-default-rtdb.firebaseio.com/'+keys[0]+'.json'
+requests.delete(link_delete)
+req = requests.get(link)
+print(req.json())
+
+# Put data - PUT request - Update data replacing the previous all data
+'''data = {'name': 'Pedro', 'age': 16}
+req = requests.put(link , data=json.dumps(data))
+req = requests.get(link) 
+print(req.json())'''
+
+
+# Head data - HEAD request
+
+
+# Options data - OPTIONS request
+
+# Trace data - TRACE request
+
+
